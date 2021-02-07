45d3d6608a2f2fbd9e009d2c9f6d1861a0e2e693
yonghyen <ankim2son@gmail.com>
2021-02-08 07:07:18 +0900

    회원가입

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
 const app = express();
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//allpication/json
+app.use(bodyParser.json());
 mongoose.connect('mongodb+srv://root:1234@cluster0.cu0gf.mongodb.net/test?retryWrites=true&w=majority',{
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify: true
 }).then(() => console.log('connect success'))
@@ -12,4 +19,19 @@ const port  =3000;
 app.get('/', function(req, res) {
   res.send('hello world 안녕하세요');
 });
+
+//회원 가입 할때 필요한 정보들을 client에서 가져오면 db에 넣어준다.
+app.post('/register',(req, res) =>{  
+    const user = new User(req.body);
+    user.save((err,userInfo) =>{
+        if(err) return res.json({ success : false, err});
+        return res.status(200).json({
+            success : true
+        })
+    });
+
+})
+
+
+
 app.listen(port, () => console.log(`example app listening on port ${port}!`));
