eaeb659e809a8a6470196fa9fccff7916d49becc
yonghyen <ankim2son@gmail.com>
2021-02-08 20:53:38 +0900

    암호(bcrypt) 설정

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ app.get('/', function(req, res) {
 
 //회원 가입 할때 필요한 정보들을 client에서 가져오면 db에 넣어준다.
 app.post('/register',(req, res) =>{  
+
     const user = new User(req.body);
+
     user.save((err,userInfo) =>{
         if(err) return res.json({ success : false, err});
         return res.status(200).json({
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const userSchema = mongoose.Schema({
     name: {
         type : String,
@@ -29,6 +32,22 @@ const userSchema = mongoose.Schema({
         type : Number
     }
 });
+
+userSchema.pre('save',function(next){
+    let user =this;
+    if(user.isModified('password')){
+    //비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    }
+
+})
+
 
 const User = mongoose.model('User', userSchema);
 module.exports ={User};
